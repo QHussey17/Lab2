40518e31d25c87a523cba1bfea38adaeef9eb24e
QHussey17 <112183228+QHussey17@users.noreply.github.com>
2022-09-27 09:08:01 -0500

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,20 @@
+# Exercise One
+temp = int(input("Enter a Temperature: "))
+tempF = temp * (9/5) + 32
+print("The temperature is: " + str(tempF) + " degrees fahrenheit")
+
+# Exercise Two
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+average = (num1 + num2) / 2
+print("The average is " + str(average))
+
+# Exercise Three
+print('3+4')
+print('The value of 3+4 is', 3+4)
+print('The value of 3+4 is', 3+4, ' . ')
+print('The value of 3+4 is', 3+4, ' . ', sep= '' )
+print('On the first line')
+print('On the second line')
+print('On the first line', end= ' ' )
+print( 'On the second line' )
